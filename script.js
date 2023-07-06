016c17da40ec304376e7ba786f107c79054ba0ae
Utkarsh Choudhary <72205579+UtkarshChoudhary19@users.noreply.github.com>
2023-07-06 12:02:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
-function volume_sphere() {
+function volume_sphere(event) {
+    event.preventDefault();
     //Write your code here
-	let radius = document.getElementById("radius").value;
-	let volume = (4/3) * Math.PI * Math.pow(radius, 3);
+    let radius = document.getElementById("radius").value;
+    let volume = (4/3) * Math.PI * Math.pow(radius, 3);
+    // Update the volume field with the calculated volume
+    // ...
+}
 
-	let submit  = document.getElementById("submit");
-	submit.onclick = volume_sphere;
-  
-} 
-
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+window.onload = function() {
+    let submit  = document.getElementById("submit");
+    submit.onclick = volume_sphere;
+    document.getElementById('MyForm').onsubmit = volume_sphere;
+};
