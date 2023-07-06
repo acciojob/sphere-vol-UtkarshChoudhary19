86d868bf9517625722794bf145466a06e393e1f2
Utkarsh Choudhary <72205579+UtkarshChoudhary19@users.noreply.github.com>
2023-07-06 11:58:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function volume_sphere() {
     //Write your code here
+	let radius = document.getElementById("radius").value;
+	let volume = (4/3) * Math.PI * Math.pow(radius, 3);
+
+	let submit  = document.getElementById("submit");
+	submit.onclick = volume_sphere;
   
 } 
 
